f9dd1c087d04d18ad733c9b9cd5ab328c5b36e66
henry cheche <henry.cheche@student.moringaschool.com>
2023-03-27 14:45:50 +0300

    fixes

--- src/index.js
+++ src/index.js
@@ -1,65 +1,61 @@
 // Your code here
-//Fetch Character Names
-function fetchCharacterNames() {
-  return fetch(`http://localhost:3000/characters`).then((response) => response.json());
+// get elements from the index.html
+const characterBar = document.getElementById("character-bar");
+const detailedInfo = document.getElementById("detailed-info");
+const namePar = document.getElementById("name");
+const image = document.getElementById("image");
+const voteCount = document.getElementById("vote-count");
+const voteForm = document.getElementById("votes-form");
+
+let currentCharacter;
+
+voteForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // add votes and show results
+  currentCharacter.votes += parseInt(e.target.votes.value);
+  showInfo(currentCharacter);
+
+});
+// fetch characters from db.json
+fetch("http://localhost:3000/characters")
+  .then((res) => res.json())
+  .then(renderCharacters);
+
+function renderCharacters(characters) {
+    // display all characters
+  characters.forEach(renderCharacter);
 }
-function renderCharacterNames(character) {
-  const characterBar = document.getElementById("character-bar");
-  // create span in webpage
-  const span = document.createElement("span");
-  // add character name to span
-  span.innerHTML = character.name;
-  characterBar.appendChild(span);
-  span.id = character.id;
-  // add event listener click to span
-  span.addEventListener("click", onSpanCharacterClick);
-}
-// display all character names
-fetchCharacterNames().then((characters) => {
-  characters.forEach((character) => {
-    renderCharacterNames(character);
+
+function renderCharacter(character) {
+  // Create span element
+  const characterSpan = document.createElement("span");
+  characterSpan.innerText = character.name;
+  // place span under character-bar
+  characterBar.appendChild(characterSpan);
+  // add event listener click that shows the character information
+  characterSpan.addEventListener("click", () => {
+    currentCharacter = character;
+    showInfo(character);
   });
-});
-
-//Fetching character details with id
-function fetchCharacterDetails(id) {
-  return fetch(`http://localhost:3000/characters/${id}`).then((response) => response.json());
-}
-// targets span.id
-function onSpanCharacterClick(event) {
-  fetchCharacterDetails(event.target.id).then(renderCharacterDetails);
 }
 
-function renderCharacterDetails(character) {
-  
-  //changes name depending on id
-  const charName = document.getElementById("name");
-  charName.innerText = character.name;
-// changes image depending on id
-  const charImg = document.getElementById("image");
-  charImg.src = character.image;
-// changes votes depending on id
-  const charVotes = document.getElementById("vote-count");
-  charVotes.innerText = character.votes;
+function showInfo(character) {
+    // shows character name
+  namePar.innerText = character.name;
+  // displays character image
+  image.src = character.image;
+  voteCount.innerText = character.votes;
+  fetch('http://localhost:3000/characters/1', {
+  method: 'PATCH',
+  body: JSON.stringify({
+    votes: voteCount,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
 }
 
-//Form Submission and Updating Votes
-document.getElementById("votes-form").addEventListener("submit", (event) => {
-  event.preventDefault();
-  const votesForm = event.target;
-  const votes = document.getElementById("vote-count");
-  // adds votes and gives total
-  votes.innerText = parseInt(votesForm.votes.value) + parseInt(votes.innerText);
-  votesForm.reset();
-});
 
-//Reset Button Functionality
-document.getElementById("reset-btn").addEventListener("click", () => {
 
-  document.getElementById("vote-count").innerText = 0;
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  fetchCharacterNames();
-  fetchCharacterDetails();
-});
